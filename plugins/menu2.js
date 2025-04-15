63170f6afa07c3e94b89db0109c100ab7f3cbd4e
Sh3llah <sh3llahcyber7@gmail.com>
2025-04-15 18:41:23 +0200

    Add files via upload

---
+++ plugins/menu2.js
@@ -0,0 +1,116 @@
+/**
+ Bot Name Zɪᴍ Cʏʙᴇʀ-Mᴅ
+Bot owner Mʀ Sʜᴇʟʟᴀʜ Oғᴄ
+owner number+263771341158
+ Copyright (C) 2025.
+ Licensed under the  GPL-3.0 License;
+ You may not sell this script.
+ It is supplied in the hope that it may be useful.
+ * @project_name : WhatsApp bot
+ * @author : <https://github.com/Sh3llah>
+ * @description : A Multi-functional whatsapp bot script.
+ * @version 3.0.0
+ **/
+
+
+
+
+
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "menu2",
+    react: "👾",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `*┋* ${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = `*╭─────────────────❒⁠⁠⁠⁠*
+
+*⇆ ʜɪ ᴍʏ ᴅᴇᴀʀ ғʀɪᴇɴᴅ ⇆*
+
+     *${pushname}*
+
+┏━━━━━━━━━━━━━━━━━━━━
+   *Welcome to Botname 2nd Menu*
+┗━━━━━━━━━━━━━━━━━━━━
+
+*Created by sʜᴇʟʟᴀʜ ᴏғᴄ👨🏻‍💻*
+
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂ DOWNLOAD CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.download}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂MAIN CMDS❂*
+*┕───────────────❒*
+*╭──────────●●►*
+${menu.main}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂GROUP CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.group}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂OWNER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.owner}
+*╰──────────●●►*
+
+*╭───────────────❒⁠⁠⁠⁠*
+*│* *❂CONVERTER CMDS❂*
+*┕───────────────❒*
+
+*╭──────────●●►*
+${menu.convert}
+*╰──────────●●►*
+
+*╭─────────────────❒⁠⁠⁠⁠*
+*│* *❂SEARCH CMDS ❂*
+*┕─────────────────❒*
+
+*╭──────────●●►*
+${menu.search}
+*╰──────────●●►*
+
+
+> *Powered by Zɪᴍ Cʏʙᴇʀ Mᴅ*
+
+`;
+
+await conn.sendMessage(from,{image:{url:config.MENU_IMG},caption:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
